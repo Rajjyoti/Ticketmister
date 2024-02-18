b8b7bf91056b496abf5faae3e6fec8dd8159e6b8
Raj Doley <rajdoley@Rajs-MacBook-Pro.local>
2024-02-18 12:09:01 +0530

    user ruotes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,16 @@
 import express from 'express'
 import {json} from 'body-parser'
 
+import { userRouter } from './routes/user'
+
 const app = express()
-app.use(json)
+app.use(json())
+
+app.use(userRouter);
+
+app.get('/api/users', (req, res) => {
+    res.send("Hi there!!")
+})
 
 app.listen(3000, () => {
     console.log("Listening on port 3000!!!!")
