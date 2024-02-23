9e2d12a349c047365908660c2432d9526f6c38a0
Raj Doley <rajdoley@Rajs-MacBook-Pro.local>
2024-02-23 18:53:00 +0530

    ticket routes

--- tickets/src/routes/ticket.ts
+++ tickets/src/routes/ticket.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from 'express'
 import { body } from 'express-validator'
-import { requireAuth, validateRequest } from '@rjdtickets/commons'
+import { NotAuthorisedError, NotFoundError, requireAuth, validateRequest } from '@rjdtickets/commons'
 import { Ticket } from '../models/ticket'
 
 const router = express.Router()
@@ -26,6 +26,51 @@ router.post('/api/tickets', requireAuth, [
 
     res.status(201).send(ticket)
 
+})
+
+router.get('/api/tickets', requireAuth, async (req: Request, res: Response) => {
+    const tickets = await Ticket.find({})
+
+    res.send(tickets) //default 200
+})
+
+router.get('/api/tickets/:id', requireAuth, async (req: Request, res: Response) => {
+    const ticket = await Ticket.findById(req.params.id)
+
+    if (!ticket) {
+        throw new NotFoundError()
+    }
+
+    res.send(ticket) //default 200
+})
+
+router.put('/api/tickets/:id', requireAuth, [
+    body('title')
+        .not()
+        .isEmpty()
+        .withMessage('Title is required'),
+    body('price')
+        .isFloat({gt: 0})
+        .withMessage('Price must be greater than 0')
+], validateRequest, async (req: Request, res: Response) => {
+    const ticket = await Ticket.findById(req.params.id)
+
+    if (!ticket) {
+        throw new NotFoundError()
+    }
+
+    if (ticket.userId !== req.currentUser!.id) {
+        throw new NotAuthorisedError()
+    }
+
+    ticket.set({
+        title: req.body.title,
+        price: req.body.price
+    })
+
+    await ticket.save()
+
+    res.send(ticket) //default 200
 })
 
 export {router as ticketRouter}
