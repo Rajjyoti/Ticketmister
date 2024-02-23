daf2fe3deb325c21c7fcc9cdad06205d0941016f
Raj Doley <rajdoley@Rajs-MacBook-Pro.local>
2024-02-23 10:48:13 +0530

    refactor

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 import Router from "next/router"
 import useRequest from "../../hooks/use-request"
 
-const Signup = () => {
+const SignIn = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const {doRequest, errors} = useRequest({
@@ -36,4 +36,4 @@ const Signup = () => {
     </form>
 }
 
-export default Signup
+export default SignIn
