c8d589aa51e6676ad453dc7bb5c7458c4d64883a
Raj Doley <rajjyotidoley1@gmail.com>
2024-03-17 10:50:18 +0530

    event listeners and versioning

---
+++ orders/src/events/listeners/ticketUpdatedListener.ts
@@ -0,0 +1,24 @@
+import { Listener, NotFoundError, Subjects, TicketUpdatedEvent } from "@rjdtickets/commons";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queueGroupName";
+import { Ticket } from "../../models/ticket";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+    subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+    queueGroupName = queueGroupName
+
+    async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+
+        const ticket = await Ticket.findById(data.id)
+        if (!ticket) {
+            throw new Error('Ticket Not Found')
+        }
+
+        const {title, price} = data
+        ticket.set({title, price})
+        await ticket.save()
+
+        msg.ack()
+    }
+
+}
