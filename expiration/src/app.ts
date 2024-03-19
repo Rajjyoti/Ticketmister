1207927abcaf01d6c1ec3e71effb2b3baafb9ac0
Raj Doley <rajjyotidoley1@gmail.com>
2024-03-19 21:30:27 +0530

    expiration service initial setup

---
+++ expiration/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import 'express-async-errors'
+import {json} from 'body-parser'
+import cookieSession from 'cookie-session'
+
+
+import { errorHandler, NotFoundError, currentUser } from '@rjdtickets/commons'
+
+const app = express()
+app.set('trust proxy', true)
+app.use(json())
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}))
+
+app.use(currentUser)
+
+app.all('*', async(req, res) => {
+    throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export {app}
